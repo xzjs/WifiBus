16307d7d8b42036729061cfffe51c78de38fac38
yxg <yanxiguoa@126.com>
2015-09-15 09:42:49 +0800

    Revert "数据库配置、新增 Line的增删查改"
    This reverts commit 5f25a048d5fe9cb8ee000cfe983c849ff3dc1a8d.

--- Application/Home/Model/LineModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Model;
-use Think\Model;
-class LineModel extends Model {
-    // 定义自动验证
-    protected $_validate    =   array(
-        array('name','require','线路名不能为空！'),
-        );
- }
