227c7a733e968098142840f8af32197cdff332a7
xzjs <2310976219@qq.com>
2015-09-16 19:56:11 +0800

    管理员显示列表完成

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -22,9 +22,52 @@ class AdminController extends Controller
         $o=$a->login($data);
         if($o){
             $_SESSION['admin']=$o;
-            var_dump($o);
+            //var_dump($o);
+            $this->success('登陆成功','adminlist');
         }else{
             $this->error('登录失败');
         }
+    }
+
+    /**
+     * 添加管理员
+     */
+    public function add(){
+        $Admin=M('Admin');
+        if($Admin->create()){
+            $Admin->pwd=md5('1');
+            $Admin->type='Low';
+            $a=$_SESSION['admin'];
+            $result=$a->add($Admin->data());
+            if($result>0){
+                $this->success('添加成功','adminlist');
+            }else{
+                $this->error('添加失败');
+            }
+        }
+    }
+
+    /**
+     * 显示管理员列表
+     */
+    public function adminlist(){
+        $a=$_SESSION['admin'];
+        $list=$a->select();
+        $this->assign('list',$list);
+        $this->show();
+    }
+
+    /**
+     * 创建和修改
+     * @param int $id 要修改的管理员id
+     */
+    public function edit($id=0){
+        $data['id']=0;
+        if($id!=0){
+            $a=$_SESSION['admin'];
+            $data=$a->select($id);
+            $this->assign('data',$data);
+        }
+        $this->show();
     }
 }
--- Application/Home/Model/AdminModel.class.php
+++ Application/Home/Model/AdminModel.class.php
@@ -10,5 +10,11 @@ namespace Home\Model;
 use Think\Model;
 
 class AdminModel extends Model{
-
+    /**
+     * 定义验证规则
+     * @var array 用户名不可重复
+     */
+    protected $_validate=array(
+        array('name','unique','用户名重复')
+    );
 }
