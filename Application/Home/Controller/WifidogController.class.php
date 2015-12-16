1f1194e1a33b1820d6ea69f15a952879ba087784
xzjs <2310976219@qq.com>
2015-12-16 19:59:02 +0800

    增加wifidog控制器

---
+++ Application/Home/Controller/WifidogController.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xzjs
+ * Date: 15/12/16
+ * Time: 下午5:06
+ */
+namespace Home\Controller;
+
+use Think\Controller;
+
+/**
+ * Wifidog控制器
+ * @package Home\Controller
+ */
+class WifidogController extends BaseController
+{
+    public function index()
+    {
+
+    }
+
+    public function login(){
+        $this->auth();
+    }
+
+    public function ping(){
+
+    }
+
+    public function auth(){
+        echo "Auth: 1";
+    }
+}
