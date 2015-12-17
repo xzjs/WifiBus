77bcfd57f69c78b890a0e79c6a46a41fd3d74936
xzjs <2310976219@qq.com>
2015-12-17 16:29:30 +0800

    修改wifidog认证bug

--- Application/Home/Controller/WifidogController.class.php
+++ Application/Home/Controller/WifidogController.class.php
@@ -35,4 +35,30 @@ class WifidogController extends BaseController
     public function portal(){
         $this->redirect('http://www.baidu.com','location',302);
     }
+
+    public function gw_message()
+    {
+        if (isset($_REQUEST["message"])) {
+            echo $_REQUEST['message'];
+            switch ($_REQUEST["message"]) {
+                case 'failed_validation':
+                    //auth的stage为login时，被服务器返回AUTH_VALIDATION_FAILED时，来到该处处理
+                    //认证失败，请重新认证
+                    break;
+                case 'denied':
+                    //auth的stage为login时，被服务器返回AUTH_DENIED时，来到该处处理
+                    //认证被拒
+                    break;
+                case 'activate':
+                    //auth的stage为login时，被服务器返回AUTH_VALIDATION时，来到该处处理
+                    //待激活
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            //不回显任何信息
+            echo '出错了';
+        }
+    }
 }
