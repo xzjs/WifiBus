d3bdafc28cb4727f20b79e88ef1b62e986e52e08
rachel-sun <sunyx456@163.com>
2015-12-18 21:35:56 +0800

    sun第一次提交

--- Application/Home/Controller/WifidogController.class.php
+++ Application/Home/Controller/WifidogController.class.php
@@ -16,8 +16,6 @@ use Think\Controller;
 class WifidogController extends BaseController
 {
     public function login($gw_port,$gw_address){
-        echo $gw_port;
-        echo $gw_address;
         $this->assign('url',"http://$gw_address:$gw_port/wifidog/auth?token=".time());
         $this->show();
     }
@@ -32,31 +30,5 @@ class WifidogController extends BaseController
 
     public function portal(){
         redirect('http://www.baidu.com',3);
-    }
-
-    public function gw_message()
-    {
-        if (isset($_REQUEST["message"])) {
-            echo $_REQUEST['message'];
-            switch ($_REQUEST["message"]) {
-                case 'failed_validation':
-                    //auth的stage为login时，被服务器返回AUTH_VALIDATION_FAILED时，来到该处处理
-                    //认证失败，请重新认证
-                    break;
-                case 'denied':
-                    //auth的stage为login时，被服务器返回AUTH_DENIED时，来到该处处理
-                    //认证被拒
-                    break;
-                case 'activate':
-                    //auth的stage为login时，被服务器返回AUTH_VALIDATION时，来到该处处理
-                    //待激活
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            //不回显任何信息
-            echo '出错了';
-        }
     }
 }
