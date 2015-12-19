5609870d5a545b6ca94781ad00a91b0c45ec3965
yxg <yanxiguoa@126.com>
2015-12-19 10:57:34 +0800

    更新

--- Application/Home/Model/AdModel.class.php
+++ Application/Home/Model/AdModel.class.php
@@ -2,6 +2,12 @@
 namespace Home\Model;
 use Think\Model;
 class AdModel extends Model {
-    // 定义自动验证
+    protected $_validate = array(
+			array('type','require','用户id必须'),
+	);
+    
+    protected $_auto = array (
+    		array('text','time',2,'function'), // 对update_time字段在更新的时候写入当前时间戳
+    );
     
  }
