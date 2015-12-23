1d6e324eb00a1edda57c0ce5c40037337e31093b
rachel <543751087@qq.com>
2015-12-23 12:39:10 +0800

    bus修改成不影响你的了，西国

--- Application/Home/Controller/BusController.class.php
+++ Application/Home/Controller/BusController.class.php
@@ -100,7 +100,7 @@ class BusController extends BaseController {
 			} elseif ($line_id != 0) {//根据线路搜索
 				$map ['line_id'] = $line_id;
 			}
-			$data = $Bus->where ( $map )->field ( 'id as carId,no as carNum' )->order('no')->select ();
+			$data = $Bus->where ( $map )->field ( 'id ,no ' )->order('no')->select ();
 			$a = json_encode ( $data );
 			
 			$this->ajaxReturn ( json_encode ( $data ) );
