8eb260e04b420083ba4d0b2c31c9251794a1d752
rachel <543751087@qq.com>
2015-12-26 18:14:04 +0800

    ad_time +左边栏

--- Application/Home/Controller/AnalyseController.class.php
+++ Application/Home/Controller/AnalyseController.class.php
@@ -125,40 +125,7 @@ class AnalyseController extends Controller {
 		echo json_encode($array);
 		
 	}
-	/**
-	 * 线路和车辆条件下查询时间与广告点击量的关系
-	 * @param number $line_id
-	 * @param number $bus_id
-	 */
-	public function get_ad_click($line_id=0,$bus_id=0) {
-		/*//$Ad=D('Ad');
-		//$data = $Ad->where('line_id='.$line_id)->field('text,click_num')->order('click_num desc')->limit(6)->select();
-		$time=time()-6*86400;
-		//$result=M()->query("SELECT  num,TIME FROM think_adclick WHERE TIME>(UNIX_TIMESTAMP(NOW())-6*86400)");
-		$MediaModel=D('Media');
-		$array=array();
-	    $base=A('Base');
-	    $today=$base->weekday(strtotime("now "));
-	//f "d".$today;
-	for ($h = 0; $h <7; $h++) {
-		$sum[$h] = 0;
-	}
-	for($i=0;$i<count($result);$i++){
-		$dif= (int)((strtotime("now ")-(strtotime(date('Y-m-d', $result [$i] ['time']))))/86400);
-		$sum[$dif]=$sum[$dif]+$result [$i]['num'];
-			
-		}
-			for($i=0;$i<7;$i++){
 	
-			$array[$i]=array(
-					'time'=>"周".mb_substr(($base->weekday((strtotime("now ")-(86400*$i)))),-1),
-	             	'num'=>$sum[$i],
-			);
-	//echo 	$sum[$i];
-		}
-		echo json_encode($array);*/
-
-	}
 	public function index() {
 		$this->assign('title','广告和流量分析');
         $this->assign('class2','action');
@@ -185,7 +152,58 @@ class AnalyseController extends Controller {
 		$this->assign('class2','action');
 		$this->display ();
 	}
+	/**
+	 * 线路和车辆条件下查询时间与广告点击量的关系
+	 * @param number $line_id
+	 * @param number $bus_id
+	 */
+	public function get_ad_click() {
+		$line_id = I('post.line_id');
+		$bus_id =I('post.bus_id');
+		if ($bus_id == 0 && $line_id == 0) {
+			$sql="SELECT SUM(mac.click_num) AS click_num,md.text,mac.time FROM think_media AS md,think_mediaclick AS mac 
+WHERE mac.media_id=md.id AND mac.time>(UNIX_TIMESTAMP(NOW())-6*86400)
+GROUP BY mac.media_id ORDER BY  click_num DESC LIMIT 6";
+		}
+		else {
+			if ($bus_id == 0) {
+				$sql="SELECT SUM(mac.click_num) AS click_num,md.text,mac.time FROM think_media AS md,think_mediaclick AS mac 
+                        WHERE mac.media_id=md.id   AND mac.media_id IN(SELECT media_id FROM think_device_media WHERE device_id IN (SELECT  think_device.id FROM think_device,think_bus,think_line
+                       WHERE think_line.id=think_bus.line_id AND think_line.id=$line_id AND think_device.bus_id=think_bus.id)
+                               )  AND mac.time>(UNIX_TIMESTAMP(NOW())-6*86400) GROUP BY mac.media_id ORDER BY  click_num DESC LIMIT 6";
+			}
+			else{
+				$sql="  SELECT SUM(mac.click_num) AS click_num,md.text ,mac.time FROM think_media AS md,think_mediaclick AS mac 
+     WHERE mac.media_id=md.id   AND mac.media_id IN(SELECT think_device_media.media_id FROM think_device_media ,think_device 
+                 WHERE think_device_media.device_id=think_device.id AND think_device.bus_id=$bus_id)  AND mac.time>(UNIX_TIMESTAMP(NOW())-6*86400) GROUP BY mac.media_id ORDER BY  click_num DESC LIMIT 6";
+			}
+		}
+		$result=M()->query($sql);
+		//$MediaModel=D('Media');
+		$array=array();
+		$base=A('Base');
+		$today=$base->weekday(strtotime("now "));
+		//f "d".$today;
+		for ($h = 0; $h <7; $h++) {
+			$sum[$h] = 0;
+		}
+		for($i=0;$i<count($result);$i++){
+			$dif= (int)((strtotime("now ")-(strtotime(date('Y-m-d', $result [$i] ['time']))))/86400);
+			$sum[$dif]=$sum[$dif]+$result [$i]['click_num'];
+				
+		}
+		for($i=0;$i<7;$i++){
+		
+			$array[$i]=array(
+					'time'=>"周".mb_substr(($base->weekday((strtotime("now ")-(86400*$i)))),-1),
+					'num'=>$sum[$i],
+			);
+			//echo 	$sum[$i];
+		}
+		echo json_encode($array);
+		
 	
+	}
 public function get_ad_click_top() {
 		 $line_id = I('post.line_id');
 		$bus_id =I('post.bus_id');
--- Public/js/Analyse.js
+++ Public/js/Analyse.js
@@ -2,16 +2,37 @@
 $(document).ready(
 		function(){
 			get_ad_click_top(0,0);
+			get_ad_click(0,0);
 			
 		}
 		);
 function getAdInfo_line(line_id){
 	get_ad_click_top(line_id,0);
-	
+	get_ad_click(line_id,0);
 }
 function getAdInfo_bus(bus_id){
 	get_ad_click_top(0,bus_id);
-	
+	get_ad_click(0,bus_id);
+}
+function get_ad_click(line_id,bus_id){
+	$.ajax({
+		type : "POST",
+		url : "../Analyse/get_ad_click" ,
+		data:{line_id:line_id,bus_id:bus_id},
+		success : function(data) {
+			adInfo = eval(data);
+			
+			 var timeList=new Array();
+			var numList=new Array();
+			for (var i = 0; i < adInfo.length; i++) {
+				timeList[i]=adInfo[i].time;
+				numList[i]=adInfo[i].num;
+			}
+			//alert(numList);
+		    var adMainOption_adtime = adMainTimeFunction(timeList,numList);
+		    myChartAdTime.setOption(adMainOption_adtime ); 
+		}
+	});
 }
 function get_ad_click_top(line_id,bus_id){
 	//alert(line_id);
