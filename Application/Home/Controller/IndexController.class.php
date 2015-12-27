171475d04d0e5b011e787ffe792300e773bda657
rachel <543751087@qq.com>
2015-12-27 17:07:29 +0800

    所有车信息图地图显示

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -57,32 +57,29 @@ class IndexController extends Controller {
     }
 
     public function all(){
-        $data=array(
-            array(120.240311,36.055683),//黄岛轮渡
-            array(120.004914,35.877262),//体育场
-            array(120.178963,36.018485),//青岛育人医院
-            array(120.172229,35.980938),//青职学院
-            array(120.198808,35.961895),//城市桂冠
-            array(120.21714,35.965749),//理工大学
-            array(120.235136,35.971364), //嘉陵江东路
-            array(120.240311,36.055683),//黄岛轮渡
-            array(120.172229,35.980938),//青职学院
-            array(120.21714,35.965749),//理工大学
-            array(120.198808,35.961895),//城市桂冠
-            array(120.179246,35.970552),//老年大学
-            array(120.250947,35.966188),//金沙滩
-            array(120.21714,35.965749),//理工大学
-            array(120.198808,35.961895),//城市桂冠
-            array(120.250947,35.966188),//金沙滩
-            array(120.191441,35.958657),//家佳源
-            array(120.295569,35.995049),//隧道薛家岛枢纽站
-            array(120.132005,35.923543),//灵山卫公交枢纽站
-            array(120.093147,36.098653),//龙泉王家
-            array(120.198808,35.961895),//城市桂冠
-            array(120.178963,36.018485)//青岛育人医院
-        );
-        $arr['data']=$data;
-        echo json_encode($arr);
+    	$result=M()->query("SELECT think_device.time, think_device.id,think_bus.position_x,think_bus.position_y,think_bus.no,think_device.online_num,think_device.flow_num,think_device.useage FROM think_device,think_bus WHERE think_device.bus_id=think_bus.id 
+    			");
+    	$time=time();
+    	for($i=0;$i<count($result);$i++){
+    		if(($time-$result[$i]['time'])>30*60)
+    		{
+    			$flag=1;//失联了
+    		}
+    		else {
+    			$flag=0;
+    		}
+    		$array[$i]=array(
+    				'flag'=>$flag,
+    				'car_no'=>$result[$i]['no'],
+    				'online_num'=>$result[$i]['online_num'],
+    				'flow_num'=>$result[$i]['flow_num'],
+    				'cipan_use'=>$result[$i]['useage'],
+    				'p_x'=>$result[$i]['position_x'],
+    				'p_y'=>$result[$i]['position_y'],
+    		);
+    	}
+    	echo json_encode($array);
+      	
     }
 
     /**
