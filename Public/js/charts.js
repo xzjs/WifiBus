20900ee12bba4bd6d4cc21696a0db9f2f5a4f007
rachel <543751087@qq.com>
2016-01-06 14:35:53 +0800

    再此提交

--- Public/js/charts.js
+++ Public/js/charts.js
@@ -956,7 +956,6 @@ function adver_rankOptionFunction1(time,value){
             }
         ],
         series: [
-                 
             {
                 name: '连接人次',
                 type: 'line',
