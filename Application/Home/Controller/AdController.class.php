dfb79854e01f38df0b2de16385f26714fb8f96fd
xzjs <2310976219@qq.com>
2016-05-06 09:46:10 +0800

    增加添加广告的功能

--- Application/Home/Controller/AdController.class.php
+++ Application/Home/Controller/AdController.class.php
@@ -13,21 +13,23 @@ use Think\Controller;
  */
 class AdController extends Controller
 {
-	/**
-	 * “广告设置”页面
-	 */
-	public function index(){
-		$this->assign('title','广告设置');
-		$this->assign('class4','action');
-		$Line = A ( 'Line' );
-		$data = $Line->getLineList ();
-		$this->assign ( 'line_list', $data );
-		$Bus=A('Bus');
-		$data = $Bus->select(0,$data[0][id],'',0);
-		$this->assign ( 'bus_list', $data );
-		
-		$this->display ();
-	}
+    /**
+     * “广告设置”页面
+     */
+    public function index()
+    {
+        $this->assign('title', '广告设置');
+        $this->assign('class4', 'action');
+        $Line = A('Line');
+        $data = $Line->getLineList();
+        $this->assign('line_list', $data);
+        $Bus = A('Bus');
+        $data = $Bus->select(0, $data[0][id], '', 0);
+        $this->assign('bus_list', $data);
+
+        $this->display();
+    }
+
     /**
      * 添加广告
      */
@@ -57,23 +59,23 @@ class AdController extends Controller
     public function select()
     {
         $Ad = M('Ad');
-        $lineid=I('param.line_id');
-    	if(I('param.line_id',0)!=0){
-    		$result=$Ad->where('line_id=%d',$lineid)->order('click_num desc')->limit(6)->select();
-    		$this->ajaxReturn($result);
-    	}else{
-    		if ($id != 0)
-    			$condition ['id'] = $id;
-    		if ($type != 0)
-    			$condition ['type'] = $type;
-    		$result = $Ad->where($condition)->select();
-    		if ($result) {
-    			var_dump($result);
-    		} else {
-    			$this->error('查询失败！');
-    		}
-    	}
-        
+        $lineid = I('param.line_id');
+        if (I('param.line_id', 0) != 0) {
+            $result = $Ad->where('line_id=%d', $lineid)->order('click_num desc')->limit(6)->select();
+            $this->ajaxReturn($result);
+        } else {
+            if ($id != 0)
+                $condition ['id'] = $id;
+            if ($type != 0)
+                $condition ['type'] = $type;
+            $result = $Ad->where($condition)->select();
+            if ($result) {
+                var_dump($result);
+            } else {
+                $this->error('查询失败！');
+            }
+        }
+
     }
 
     /**
@@ -142,38 +144,41 @@ class AdController extends Controller
             $this->error('删除失败！');
         }
     }
-    
-    public function get_img($ids=0) {
-    	$Media=A('Media');
-    	$img_list=$Media->get_img();
-    	for($i=0;$i<count($img_list);$i++){
-    		$img_url[$i]['img']='http://'.GetHostByName($_SERVER['SERVER_NAME']).'/WifiBus/Uploads/'.$img_list[$i]['url'];
-    		$img_url[$i]['text']=$img_list[$i]['text'];
-    	}
-    	$this->ajaxReturn($img_url);
-    }
-    
-    public function upload() {
-    	$a=I('param.img_dsc');
-    	$b=I('param.text_dsc');
-    	$this->ajaxReturn($a);
+
+    public function get_img($ids = 0)
+    {
+        $Media = A('Media');
+        $img_list = $Media->get_img();
+        for ($i = 0; $i < count($img_list); $i++) {
+            $img_url[$i]['img'] = 'http://' . GetHostByName($_SERVER['SERVER_NAME']) . '/WifiBus/Uploads/' . $img_list[$i]['url'];
+            $img_url[$i]['text'] = $img_list[$i]['text'];
+        }
+        $this->ajaxReturn($img_url);
     }
 
-    public function get_media_info($ids) {
-        $id_list=$ids;
-        $device=M('Device');
-        $device_ids=array();
-        for($i=0;$i<count($id_list);$i++){
-            $condition['bus_id']=$id_list[$i];
-            $result=$device->where($condition)->field('id')->find();
-            if($result)
+    public function upload()
+    {
+        $a = I('param.img_dsc');
+        $b = I('param.text_dsc');
+        $this->ajaxReturn($a);
+    }
+
+    public function get_media_info($ids)
+    {
+        $id_list = $ids;
+        $device = M('Device');
+        $device_ids = array();
+        for ($i = 0; $i < count($id_list); $i++) {
+            $condition['bus_id'] = $id_list[$i];
+            $result = $device->where($condition)->field('id')->find();
+            if ($result)
                 array_push($device_ids, $result['id']);
         }
 
-        $json['ssid']=$this->get_media($device_ids);
+        $json['ssid'] = $this->get_media($device_ids);
 
 
-        $json['flow_limit']=$Device->get_network_limit($device_ids);
+        $json['flow_limit'] = $Device->get_network_limit($device_ids);
         $this->ajaxReturn($json);
     }
 
@@ -182,23 +187,24 @@ class AdController extends Controller
      * @param array $device_ids id列表
      * @return string ssid名
      */
-    public function get_media($device_ids) {
-        $position_arr=array("ad1","ad2","ad3","ad4","ad5","ad6");
-        if(count($device_ids)==0){
-            for($j=0;$j<count($position_arr);$j++){
-                $position_info[$position_arr[$j]]=null;
+    public function get_media($device_ids)
+    {
+        $position_arr = array("ad1", "ad2", "ad3", "ad4", "ad5", "ad6");
+        if (count($device_ids) == 0) {
+            for ($j = 0; $j < count($position_arr); $j++) {
+                $position_info[$position_arr[$j]] = null;
             }
-        }else{
-            for($i=0;$i<count($device_ids);$i++){
-                $device_id_str.=$device_ids[$i];
-                if($i!=count($device_ids)-1)
-                    $device_id_str.=",";
+        } else {
+            for ($i = 0; $i < count($device_ids); $i++) {
+                $device_id_str .= $device_ids[$i];
+                if ($i != count($device_ids) - 1)
+                    $device_id_str .= ",";
             }
-            for($j=0;$j<count($position_arr);$j++){
-                $sql='select m.url,m.img,m.text from think_media as m,think_device_media as dm
-							where m.id=dm.media_id AND dm.device_id IN('.$device_id_str.') AND m.position="'.$position_arr[$j].'"';
-                $result=M()->query($sql);
-                $position_info[$position_arr[$j]]=$this->get_position_info($result,count($device_ids));
+            for ($j = 0; $j < count($position_arr); $j++) {
+                $sql = 'select m.url,m.img,m.text from think_media as m,think_device_media as dm
+							where m.id=dm.media_id AND dm.device_id IN(' . $device_id_str . ') AND m.position="' . $position_arr[$j] . '"';
+                $result = M()->query($sql);
+                $position_info[$position_arr[$j]] = $this->get_position_info($result, count($device_ids));
             }
         }
         $this->ajaxReturn($position_info);
@@ -210,45 +216,44 @@ class AdController extends Controller
      * @param unknown $result
      * @param unknown $count
      */
-    public function get_position_info($result,$count) {
-        if(GetHostByName($_SERVER['SERVER_NAME'])=="127.0.0.1"){
-            $ip=GetHostByName($_SERVER['SERVER_NAME']);
-        }elseif(GetHostByName($_SERVER['SERVER_NAME'])=="192.168.4.96"){
-            $ip=GetHostByName($_SERVER['SERVER_NAME']).":48093";
-        }elseif(GetHostByName($_SERVER['SERVER_NAME'])=="192.168.4.97"){
-            $ip=GetHostByName($_SERVER['SERVER_NAME']).":48082";
+    public function get_position_info($result, $count)
+    {
+        if (GetHostByName($_SERVER['SERVER_NAME']) == "127.0.0.1") {
+            $ip = GetHostByName($_SERVER['SERVER_NAME']);
+        } elseif (GetHostByName($_SERVER['SERVER_NAME']) == "192.168.4.96") {
+            $ip = GetHostByName($_SERVER['SERVER_NAME']) . ":48093";
+        } elseif (GetHostByName($_SERVER['SERVER_NAME']) == "192.168.4.97") {
+            $ip = GetHostByName($_SERVER['SERVER_NAME']) . ":48082";
         }
 
 
-
-        if($count==1){
-            if($result[0]['url'])
-                $result[0]['url']='http://'.$ip.'/WifiBus/Update/'.$result[0]['url'];
-            if($result[0]['img'])
-                $result[0]['img']='http://'.$ip.'/WifiBus/Update/'.$result[0]['img'];
+        if ($count == 1) {
+            if ($result[0]['url'])
+                $result[0]['url'] = 'http://' . $ip . '/WifiBus/Update/' . $result[0]['url'];
+            if ($result[0]['img'])
+                $result[0]['img'] = 'http://' . $ip . '/WifiBus/Update/' . $result[0]['img'];
             return $result[0];
-        }else if(count($result)!=$count){
+        } else if (count($result) != $count) {
             return null;
-        }else{
-            $flag=false;
-            for($i=1;$i<count($result);$i++){
-                if($result[$i-1]['url']!=$result[$i]['url'])
-                {
-                    $flag=true;
+        } else {
+            $flag = false;
+            for ($i = 1; $i < count($result); $i++) {
+                if ($result[$i - 1]['url'] != $result[$i]['url']) {
+                    $flag = true;
                     break;
                 }
             }
-            if($flag)
+            if ($flag)
                 return null;
-            else{
-                if($result[0]['url'])
-                    $result[0]['url']='http://'.$ip.'/WifiBus/Update/'.$result[0]['url'];
-                if($result[0]['img'])
-                    $result[0]['img']='http://'.$ip.'/WifiBus/Update/'.$result[0]['img'];
+            else {
+                if ($result[0]['url'])
+                    $result[0]['url'] = 'http://' . $ip . '/WifiBus/Update/' . $result[0]['url'];
+                if ($result[0]['img'])
+                    $result[0]['img'] = 'http://' . $ip . '/WifiBus/Update/' . $result[0]['img'];
                 return $result[0];
             }
         }
     }
 
-    
+
 }
--- Application/Home/Controller/MediaController.class.php
+++ Application/Home/Controller/MediaController.class.php
@@ -9,40 +9,40 @@ use Think\Controller;
  * @author xiuge
  *
  */
-
 class MediaController extends BaseController
 {
 
-	public function test($aaa){
+    public function test($aaa)
+    {
         var_dump(S('abc'));
-        S('abc',$aaa,3);
-	}
+        S('abc', $aaa, 3);
+    }
 
 
-
-	/**
-	 * 上传固件升级文件
-	 */
-	public function upload_devicefile() {
-		$error_data['status']=0;
-		//$file_name=$this->upload_file();
-		$upload = new \Think\Upload(); // 实例化上传类
-		$upload->maxSize = 9999999999999; // 设置附件上传大小
-		$upload->rootPath = "./Update/"; // 设置附件上传根目录
-		$upload->autoSub = false;
-		$upload->saveName = '_' . time(); // 上传文件
-		$info = $upload->upload();
-		$file_name=$info ['file'] ['savename'];
-	    $CommandCtrl=A('Command');
-        if(I('post.mac')=="all devices"){
-            $device_list=M('Device')->field('mac')->select();
-            foreach($device_list as $vo){
-                $device_id=A('Device')->get_id($vo['mac']);
-                $cmd_result1=$CommandCtrl->add($device_id,'Firmwareupdate','/WifiBus/Update/|'.$file_name.'|'.'heartbeat');
-                if($cmd_result1>0)
+    /**
+     * 上传固件升级文件
+     */
+    public function upload_devicefile()
+    {
+        $error_data['status'] = 0;
+        //$file_name=$this->upload_file();
+        $upload = new \Think\Upload(); // 实例化上传类
+        $upload->maxSize = 9999999999999; // 设置附件上传大小
+        $upload->rootPath = "./Update/"; // 设置附件上传根目录
+        $upload->autoSub = false;
+        $upload->saveName = '_' . time(); // 上传文件
+        $info = $upload->upload();
+        $file_name = $info ['file'] ['savename'];
+        $CommandCtrl = A('Command');
+        if (I('post.mac') == "all devices") {
+            $device_list = M('Device')->field('mac')->select();
+            foreach ($device_list as $vo) {
+                $device_id = A('Device')->get_id($vo['mac']);
+                $cmd_result1 = $CommandCtrl->add($device_id, 'Firmwareupdate', '/WifiBus/Update/|' . $file_name . '|' . 'heartbeat');
+                if ($cmd_result1 > 0)
                     echo $file_name;
             }
-        }else{
+        } else {
             $device_id = A('Device')->get_id(I('post.mac'));
             //$device_idhh=$device->get_id(I('post.mac'));
             // $file_name=I('post.ye');
@@ -53,71 +53,77 @@ class MediaController extends BaseController
             //$cmd_result2=$CommandCtrl->add($device['id'],'Contentsupdate','/WifiBus/Update/|'.$name_str[0].'.jpg'.'|'.I('post.position').'.jpg');
 
         }
-	 			
-	}
-	
+
+    }
+
 
     /**
      * 添加媒体
      */
     public function add()
     {
-        $error_data['status']=0;
-        $img=$this->upload_file("ad");
+        $error_data['status'] = 0;
+        $img = $this->upload_file("ad");
         $Media = D('Media');
         if ($Media->create()) {
-            $Media->img=$img;
-            $media_id=$Media->add();
-            if($media_id){
-                $buses=explode('_',I('post.ids'));
+            $Media->img = $img;
+            $media_id = $Media->add();
+            if ($media_id) {
+                $buses = explode('_', I('post.ids'));
                 foreach ($buses as $bus) {
-                    $DeviceModel=D('Device');
-                    $condition['bus_id']=$bus;
-                    $device=$DeviceModel->where($condition)->relation(true)->find();
-                    $m='';
+                    $DeviceModel = D('Device');
+                    $condition['bus_id'] = $bus;
+                    $device = $DeviceModel->where($condition)->relation(true)->find();
+                    $m = '';
                     foreach ($device['Media'] as $media) {
-                        $m=$media['position']==I('post.position')?$media:$m;
+                        $m = $media['position'] == I('post.position') ? $media : $m;
                     }
-                    $dmModel=M('Device_media');
-                    $result=false;
-                    if($m==''){
-                        $data=array(
-                            'device_id'=>$device['id'],
-                            'media_id'=>$media_id
+                    $dmModel = M('Device_media');
+                    $result = false;
+                    if ($m == '') {
+                        $data = array(
+                            'device_id' => $device['id'],
+                            'media_id' => $media_id
                         );
-                        $result=$dmModel->add($data);
-                    }else{
-                        $condition=array(
-                            'device_id'=>$device['id'],
-                            'media_id'=>$m['id']
+                        $result = $dmModel->add($data);
+                    } else {
+                        $condition = array(
+                            'device_id' => $device['id'],
+                            'media_id' => $m['id']
                         );
-                        $dm=$dmModel->where($condition)->find();
-                        $dm['media_id']=$media_id;
-                        $result=$dmModel->save($dm);
+                        $dm = $dmModel->where($condition)->find();
+                        $dm['media_id'] = $media_id;
+                        $result = $dmModel->save($dm);
                     }
-                    if($result){
-                        $CommandCtrl=A('Command');
-                        $cmd_result=$CommandCtrl->add($device['id'],'Contentsupdate','/WifiBus/Update/|'.$img.'|'.I('post.position').'.jpg');
-                        if($cmd_result){
-                            $error_data['status']=0;
-                            $error_data['data']='成功';
-                        }else{
-                            $error_data['data']='命令插入错误';
+                    if ($result) {
+                        $CommandCtrl = A('Command');
+                        $position = substr(I('post.position'), 0, 2);
+                        if ($position == 'ad') {
+                            $cmd_result = $CommandCtrl->add($device['id'], 'AdUpdate', '/WifiBus/Update/|' . $img . '|' . I('post.position') . '.jpg|' . I('post.url') . '|' . I('post.text'));
+                        } else {
+                            $cmd_result = $CommandCtrl->add($device['id'], 'Contentsupdate', '/WifiBus/Update/|' . $img . '|' . I('post.position') . '.jpg');
                         }
-                    }else{
-                        $error_data['data']= '添加关系表失败';
+                        if ($cmd_result) {
+                            $error_data['status'] = 0;
+                            $error_data['data'] = '成功';
+                        } else {
+                            $error_data['data'] = '命令插入错误';
+                        }
+                    } else {
+                        $error_data['data'] = '添加关系表失败';
                     }
                 }
-            }else{
-                $error_data['data']= '上传失败';
+            } else {
+                $error_data['data'] = '上传失败';
             }
         } else {
-            $error_data['data']= $Media->getError();
+            $error_data['data'] = $Media->getError();
         }
         $this->ajaxReturn($error_data);
     }
 
-    public function progress(){
+    public function progress()
+    {
         session_start();
 
         $i = ini_get('session.upload_progress.name');
@@ -128,81 +134,82 @@ class MediaController extends BaseController
             $current = $_SESSION[$key]["bytes_processed"];
             $total = $_SESSION[$key]["content_length"];
             echo $current < $total ? ceil($current / $total * 100) : 100;
-        }else{
+        } else {
             echo 100;
         }
     }
-    
+
     /**
      * 上传电影、电子书、apk等文件
      */
-    public function upload( ) {
-    	$error_data['status']=0;
-        if(strstr(I('post.position'),'video')) {
-            $file_name=$this->upload_video('video');
-        }elseif(strstr(I('post.position'),'book')){
+    public function upload()
+    {
+        $error_data['status'] = 0;
+        if (strstr(I('post.position'), 'video')) {
+            $file_name = $this->upload_video('video');
+        } elseif (strstr(I('post.position'), 'book')) {
             $file_name = $this->upload_file('book');
-        }elseif(strstr(I('post.position'),'app')){
+        } elseif (strstr(I('post.position'), 'app')) {
             $file_name = $this->upload_file('app');
         }
-    	$Media = D('Media');
-    	if ($Media->create()) {
-    		$Media->url=$file_name;
-    		$name_str=explode(".",$file_name);
-    		$Media->img=$name_str[0].'.jpg';
-    		$media_id=$Media->add();
-    		if($media_id){
-    			$buses=explode('_',I('post.ids'));
-    			foreach ($buses as $bus) {
-    				$DeviceModel=D('Device');
-    				$condition['bus_id']=$bus;
-    				$devices=$DeviceModel->where($condition)->relation(true)->select();
-    				if(!$devices)
-    					continue;
-    				$device=$devices[0];
-    				$m='';
-    				foreach ($device['Media'] as $media) {
-    					$m=$media['position']==I('post.position')?$media:$m;
-    				}
-    				$dmModel=M('Device_media');
-    				$result=false;
-    				if($m==''){
-    					$data=array(
-    							'device_id'=>$device['id'],
-    							'media_id'=>$media_id
-    					);
-    					$result=$dmModel->add($data);
-    				}else{
-    					$condition=array(
-    							'device_id'=>$device['id'],
-    							'media_id'=>$m['id']
-    					);
-    					$dm=$dmModel->where($condition)->find();
-    					
-    					$dm['media_id']=$media_id;
-    					$result=$dmModel->save($dm);
-    				}
-    				if($result){
-    					$CommandCtrl=A('Command');
-    					$cmd_result1=$CommandCtrl->add($device['id'],'Contentsupdate','/WifiBus/Update/|'.$file_name.'|'.I('post.position').'.'.$name_str[1]);
-    					$cmd_result2=$CommandCtrl->add($device['id'],'Contentsupdate','/WifiBus/Update/|'.$name_str[0].'.jpg'.'|'.I('post.position').'.jpg');
-    					if($cmd_result1&&$cmd_result2){
-    						$error_data['status']=0;
-    						$error_data['data']='成功';
-    					}else{
-    						$error_data['data']='命令插入错误';
-    					}
-    				}else{
-    					$error_data['data']= '添加关系表失败';
-    				}
-    			}
-    		}else{
-    			$error_data['data']= '上传失败';
-    		}
-    	} else {
-    		$error_data['data']= $Media->getError();
-    	}
-    	$this->ajaxReturn($error_data);
+        $Media = D('Media');
+        if ($Media->create()) {
+            $Media->url = $file_name;
+            $name_str = explode(".", $file_name);
+            $Media->img = $name_str[0] . '.jpg';
+            $media_id = $Media->add();
+            if ($media_id) {
+                $buses = explode('_', I('post.ids'));
+                foreach ($buses as $bus) {
+                    $DeviceModel = D('Device');
+                    $condition['bus_id'] = $bus;
+                    $devices = $DeviceModel->where($condition)->relation(true)->select();
+                    if (!$devices)
+                        continue;
+                    $device = $devices[0];
+                    $m = '';
+                    foreach ($device['Media'] as $media) {
+                        $m = $media['position'] == I('post.position') ? $media : $m;
+                    }
+                    $dmModel = M('Device_media');
+                    $result = false;
+                    if ($m == '') {
+                        $data = array(
+                            'device_id' => $device['id'],
+                            'media_id' => $media_id
+                        );
+                        $result = $dmModel->add($data);
+                    } else {
+                        $condition = array(
+                            'device_id' => $device['id'],
+                            'media_id' => $m['id']
+                        );
+                        $dm = $dmModel->where($condition)->find();
+
+                        $dm['media_id'] = $media_id;
+                        $result = $dmModel->save($dm);
+                    }
+                    if ($result) {
+                        $CommandCtrl = A('Command');
+                        $cmd_result1 = $CommandCtrl->add($device['id'], 'Contentsupdate', '/WifiBus/Update/|' . $file_name . '|' . I('post.position') . '.' . $name_str[1]);
+                        $cmd_result2 = $CommandCtrl->add($device['id'], 'Contentsupdate', '/WifiBus/Update/|' . $name_str[0] . '.jpg' . '|' . I('post.position') . '.jpg');
+                        if ($cmd_result1 && $cmd_result2) {
+                            $error_data['status'] = 0;
+                            $error_data['data'] = '成功';
+                        } else {
+                            $error_data['data'] = '命令插入错误';
+                        }
+                    } else {
+                        $error_data['data'] = '添加关系表失败';
+                    }
+                }
+            } else {
+                $error_data['data'] = '上传失败';
+            }
+        } else {
+            $error_data['data'] = $Media->getError();
+        }
+        $this->ajaxReturn($error_data);
     }
 
     /**
@@ -306,39 +313,40 @@ class MediaController extends BaseController
      * @param $mac 设备mac
      * @param $position 位置
      */
-    public function update_click($mac,$position){
-        $DeviceModel=D('Device');
-        $condition['mac']=$mac;
-        $device=$DeviceModel->where($condition)->find();
-        $DeviceMediaModel=M('DeviceMedia');
+    public function update_click($mac, $position)
+    {
+        $DeviceModel = D('Device');
+        $condition['mac'] = $mac;
+        $device = $DeviceModel->where($condition)->find();
+        $DeviceMediaModel = M('DeviceMedia');
         unset($condition);
-        $condition['device_id']=$device['id'];
-        $media_ids=$DeviceMediaModel->where($condition)->getField('media_id');
+        $condition['device_id'] = $device['id'];
+        $media_ids = $DeviceMediaModel->where($condition)->getField('media_id');
         unset($condition);
-        $condition['id']=array('in',$media_ids);
-        $condition['position']=$position;
-        $MediaModel=D('Media');
-        $media_id=$MediaModel->where($condition)->fetchSql(false)->getField('id');
-        $MediaclickModel=M('Mediaclick');
+        $condition['id'] = array('in', $media_ids);
+        $condition['position'] = $position;
+        $MediaModel = D('Media');
+        $media_id = $MediaModel->where($condition)->fetchSql(false)->getField('id');
+        $MediaclickModel = M('Mediaclick');
         unset($condition);
-        $condition['media_id']=$media_id;
-        $condition[time]=array(array('gt',strtotime('yesterday')),array('lt',strtotime('tomorrow')));
-        $result=$MediaclickModel->where($condition)->find();
-        if($result){
-            $result['click_num']+=1;
-            $result['time']=time();
-            $update_num=$MediaclickModel->save($result);
-            if(!$update_num){
+        $condition['media_id'] = $media_id;
+        $condition[time] = array(array('gt', strtotime('yesterday')), array('lt', strtotime('tomorrow')));
+        $result = $MediaclickModel->where($condition)->find();
+        if ($result) {
+            $result['click_num'] += 1;
+            $result['time'] = time();
+            $update_num = $MediaclickModel->save($result);
+            if (!$update_num) {
                 throw_exception('修改失败');
             }
-        }else{
-            $data=array(
-                'media_id'=>$media_id,
-                'click_num'=>1,
-                'time'=>time()
+        } else {
+            $data = array(
+                'media_id' => $media_id,
+                'click_num' => 1,
+                'time' => time()
             );
-            $insert_id=$MediaclickModel->add($data);
-            if(!$insert_id){
+            $insert_id = $MediaclickModel->add($data);
+            if (!$insert_id) {
                 throw_exception('添加失败');
             }
         }
